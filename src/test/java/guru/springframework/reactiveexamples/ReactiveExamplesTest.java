c4166e4fe58d994b5ae1b107f4a2c51ede9dd583
Dmitri Zakharov <dmitri.zakharov@desjardins.com>
2022-07-05 21:36:33 -0400

    s20-329 Reactive Programming Examples - Final
    Unit test

--- src/test/java/guru/springframework/reactiveexamples/ReactiveExamplesTest.java
+++ src/test/java/guru/springframework/reactiveexamples/ReactiveExamplesTest.java
@@ -4,9 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Créé par dmitri le 2022-07-04.
@@ -25,65 +28,84 @@ public class ReactiveExamplesTest {
         Mono<Person> personMono = Mono.just(michael);
 
         //get person object from mono publisher
-        Person person = personMono.block();
-
-        // output name
-        log.info(person.sayMyName());
+        StepVerifier.create(personMono.log())
+                .consumeNextWith(person -> {
+                    assertThat(person.sayMyName()).isEqualTo("My Name is Michael Weston.");
+                })
+                .verifyComplete();
     }
 
     @Test
     public void monoTransform() throws Exception {
         //create new person mono
-        Mono<Person> personMono = Mono.just(fiona);
-
-        PersonCommand command = personMono
+        Mono<PersonCommand> personCommandMono = Mono.just(fiona)
                 .map(person -> { //type transformation
                     return new PersonCommand(person);
-                }).block();
+                });
 
-        log.info(command.sayMyName());
+        StepVerifier.create(personCommandMono.log())
+                .consumeNextWith(command -> {
+                    assertThat(command.sayMyName()).isEqualTo("My Name is Fiona Glenanne.");
+                })
+                .verifyComplete();
     }
 
-//    @Test(expected = NullPointerException.class)
-//    public void monoFilter() throws Exception {
-//        Mono<Person> personMono = Mono.just(sam);
-//
-//        //filter example
-//        Person samAxe = personMono
-//                .filter(person -> person.getFirstName().equalsIgnoreCase("foo"))
-//                .block();
-//
-//
-//        log.info(samAxe.sayMyName()); //throws NPE
-//    }
+    @Test
+    public void monoFilter() {
+        Mono<Person> personMono = Mono.just(sam)
+                .filter(person -> person.getFirstName().equalsIgnoreCase("foo"));
+
+        //filter example
+        StepVerifier.create(personMono.log())
+                .expectSubscription()
+                .expectNextCount(0)
+                .verifyComplete();
+    }
 
     @Test
-    public void fluxTest() throws Exception {
+    public void fluxTest() {
 
         Flux<Person> people = Flux.just(michael, fiona, sam, jesse);
 
         people.subscribe(person -> log.info(person.sayMyName()));
-
+        StepVerifier.create(people.log())
+                .consumeNextWith(michael -> assertThat(michael.sayMyName()).isEqualTo("My Name is Michael Weston."))
+                .consumeNextWith(fiona -> assertThat(fiona.sayMyName()).isEqualTo("My Name is Fiona Glenanne."))
+                .consumeNextWith(sam -> assertThat(sam.sayMyName()).isEqualTo("My Name is Sam Axe."))
+                .consumeNextWith(jesse -> assertThat(jesse.sayMyName()).isEqualTo("My Name is Jesse Porter."))
+                .verifyComplete();
     }
 
     @Test
-    public void fluxTestFilter() throws Exception {
+    public void fluxTestFilter() {
 
-        Flux<Person> people = Flux.just(michael, fiona, sam, jesse);
+        Flux<Person> filteredPeople = Flux.just(michael, fiona, sam, jesse)
+                .filter(person -> person.getFirstName().equals(fiona.getFirstName()));
 
-        people.filter(person -> person.getFirstName().equals(fiona.getFirstName()))
-                .subscribe(person -> log.info(person.sayMyName()));
+        StepVerifier.create(filteredPeople.log())
+                .expectSubscription()
+                .expectNext(fiona)
+                .verifyComplete();
+    }
 
+    public void fluxTestDelayWithOutput() {
+
+        Flux<Person> people = Flux.just(michael, fiona, sam, jesse)
+                .delayElements(Duration.ofSeconds(1));
+
+        StepVerifier.create(people.log())
+                .expectSubscription()
+                .expectNextCount(4)
+                .verifyComplete();
     }
 
     @Test
-    public void fluxTestDelayNoOutput() throws Exception {
+    public void fluxTestDelayNoOutput() {
 
         Flux<Person> people = Flux.just(michael, fiona, sam, jesse);
 
         people.delayElements(Duration.ofSeconds(1))
                 .subscribe(person -> log.info(person.sayMyName()));
-
     }
 
     @Test
@@ -114,5 +136,18 @@ public class ReactiveExamplesTest {
                 .subscribe(person -> log.info(person.sayMyName()));
 
         countDownLatch.await();
+    }
+
+    @Test
+    public void fluxTestFilterDelay2() {
+
+        Flux<Person> people = Flux.just(michael, fiona, sam, jesse)
+                .delayElements(Duration.ofSeconds(1))
+                .filter(person -> person.getFirstName().contains("i"));
+
+        StepVerifier.create(people.log())
+                .consumeNextWith(michael -> assertThat(michael.sayMyName()).isEqualTo("My Name is Michael Weston."))
+                .consumeNextWith(fiona -> assertThat(fiona.sayMyName()).isEqualTo("My Name is Fiona Glenanne."))
+                .verifyComplete();
     }
 }
